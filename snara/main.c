b516e6df3866c3e1ca6b7dea1583a5ec19e58eea
subaru nara <snara@student.42tokyo.jp>
2024-05-19 22:43:34 +0900

    refactor: submit

--- snara/main.c
+++ snara/main.c
@@ -40,26 +40,28 @@ int	ft_getc(t_stream *str)
 {
 	if (str->i < str->len)
 	{
-		return str->ptr[str->i++];
+		return (str->ptr[str->i++]);
 	}
-	return EOF;
+	return (EOF);
 }
+
 int	ft_ungetc(t_stream *str)
 {
 	if (str->i > 0)
 	{
-		return str->ptr[--str->i];
+		return (str->ptr[--str->i]);
 	}
-	return EOF;
+	return (EOF);
 }
+
 int	str_at(t_stream *str, ssize_t offset)
 {
 	ssize_t	i = (ssize_t)str->i + offset;
 	if (0 <= i && (size_t)i < str->len)
 	{
-		return str->ptr[i];
+		return (str->ptr[i]);
 	}
-	return EOF;
+	return (EOF);
 }
 
 void	*or_exit(void *ptr, const char *msg)
@@ -76,16 +78,17 @@ int	destroy_handler(t_image *img)
 	mlx_destroy_image(img->mlx, img->img);
 	if (img->index == 0)
 		exit(0);
-	return 0;
+	return (0);
 }
 
 int	key_handler(int key, t_image *img)
 {
+	const int	macos_escape = 53;
+
 	(void)img;
-	const int Macos_Escape = 53;
-	if (key == XK_Escape || key == Macos_Escape)
+	if (key == XK_Escape || key == macos_escape)
 		exit(0);
-	return 0;
+	return (0);
 }
 
 typedef struct {
@@ -106,36 +109,49 @@ void	display_header(t_stream *str)
 	t_header	h = {};
 	size_t		i = 0;
 
-	memcpy(h.MMF2, &data[i], sizeof(h.MMF2)); i+=sizeof(h.MMF2);
+	memcpy(h.MMF2, &data[i], sizeof(h.MMF2));
+	i += sizeof(h.MMF2);
 	if (0 != memcmp(h.MMF2, "MMF2", 4))
 		return (void)fprintf(stderr, "invalid file header: [%.*s]\n", (int)sizeof(h.MMF2), h.MMF2);
 	printf("%s\t", h.MMF2);
 
-	memcpy(h.n0, &data[i], sizeof(h.n0)); i+=sizeof(h.n0);
-	for (size_t i=0; i<sizeof(h.n0)/sizeof(*h.n0); i++) printf("%u\t", h.n0[i]);
+	memcpy(h.n0, &data[i], sizeof(h.n0));
+	i += sizeof(h.n0);
+	for (size_t i = 0; i < sizeof(h.n0) / sizeof(*h.n0); i++)
+		printf("%u\t", h.n0[i]);
 	printf("\n");
 
-	memcpy(&h.n1, &data[i], sizeof(uint32_t)); i+=sizeof(uint32_t);
-	h.s1 = (char*)&data[i]; i+=h.n1;
+	memcpy(&h.n1, &data[i], sizeof(uint32_t));
+	i += sizeof(uint32_t);
+	h.s1 = (char*)&data[i];
+	i += h.n1;
 	printf("s1:[%.*s]\n", h.n1, h.s1);
 
-	memcpy(&h.n2, &data[i], sizeof(uint32_t)); i+=sizeof(uint32_t);
-	h.s2 = (char*)&data[i]; i+=h.n2;
+	memcpy(&h.n2, &data[i], sizeof(uint32_t));
+	i += sizeof(uint32_t);
+	h.s2 = (char*)&data[i];
+	i += h.n2;
 	printf("s2:[%.*s]\n", h.n2, h.s2);
 
-	memcpy(&h.n3, &data[i], sizeof(uint32_t)); i+=sizeof(uint32_t);
-	h.s3 = (char*)&data[i]; i+=h.n3;
+	memcpy(&h.n3, &data[i], sizeof(uint32_t));
+	i += sizeof(uint32_t);
+	h.s3 = (char*)&data[i];
+	i += h.n3;
 	printf("s3:[%.*s]\n", h.n3, h.s3);
 	str->i = i;
 
-	memcpy(h.n4, &data[i], sizeof(h.n4)); i+=sizeof(h.n4);
-	for (size_t i=0; i<sizeof(h.n4)/sizeof(*h.n4); i++) printf("%08x ", h.n4[i]);
+	memcpy(h.n4, &data[i], sizeof(h.n4));
+	i += sizeof(h.n4);
+	for (size_t i = 0; i < sizeof(h.n4) / sizeof(*h.n4); i++)
+		printf("%08x ", h.n4[i]);
 	printf("\n\n");
 
-	for (size_t j=0; j<64; ++j) {
+	for (size_t j = 0; j < 64; ++j)
+	{
 		uint32_t	px;
-		memcpy(&px, &data[i], sizeof(uint32_t)); i+=sizeof(uint32_t);
-		printf("%c%08x", " \n"[j%8==0], px);
+		memcpy(&px, &data[i], sizeof(uint32_t));
+		i += sizeof(uint32_t);
+		printf("%c%08x", " \n"[j % 8 == 0], px);
 	}
 	printf("\n\n");
 }
@@ -144,7 +160,7 @@ int	seek_image(t_stream *str, t_point *size)
 {
 	for (;ft_getc(str) != EOF;)
 	{
-		if (!memcmp(&str->ptr[str->i], (uint8_t[]){0x01, 0, 0x10, 0, 0x03, 0, 0, 0}, 8))
+		if (!memcmp(&str->ptr[str->i], (uint8_t []){0x01, 0, 0x10, 0, 0x03, 0, 0, 0}, 8))
 		{
 			int	x = str_at(str, -8);
 			int	y = str_at(str, -4);
@@ -153,9 +169,9 @@ int	seek_image(t_stream *str, t_point *size)
 			*size = (t_point){x, y};
 			printf("%zd: found %d * %d thumbnail %d bit \n", str->i, size->x, size->y, 5);
 			str->i += 36;
-			return 5;
+			return (5);
 		}
-		if (!memcmp(&str->ptr[str->i], (uint8_t[]){0x07, 0x10, 0, 0}, 4))
+		if (!memcmp(&str->ptr[str->i], (uint8_t []){0x07, 0x10, 0, 0}, 4))
 		{
 			int	x = str_at(str, -4);
 			int	y = str_at(str, -2);
@@ -164,9 +180,9 @@ int	seek_image(t_stream *str, t_point *size)
 			*size = (t_point){x, y};
 			printf("%zd: found %d * %d logo %d bit \n", str->i, size->x, size->y, 5);
 			str->i += 16;
-			return 5;
+			return (5);
 		}
-		if (!memcmp(&str->ptr[str->i], (uint8_t[]){0x06, 0x10, 0, 0}, 4))
+		if (!memcmp(&str->ptr[str->i], (uint8_t []){0x06, 0x10, 0, 0}, 4))
 		{
 			int	x = str_at(str, -4);
 			int	y = str_at(str, -2);
@@ -175,22 +191,22 @@ int	seek_image(t_stream *str, t_point *size)
 			*size = (t_point){x, y};
 			printf("%zd: found %d * %d img %d bit \n", str->i, size->x, size->y, 5);
 			str->i += 16;
-			return 5;
+			return (5);
 		}
-		if (!memcmp(&str->ptr[str->i], (uint8_t[]){0x04, 0x10, 0, 0}, 4))
+		if (!memcmp(&str->ptr[str->i], (uint8_t []){0x04, 0x10, 0, 0}, 4))
 		{
 			int	x = str_at(str, -4);
 			int	y = str_at(str, -2);
 			if (x <= 0 || y <= 0)
 				continue;
-			*size = (t_point){x+(x&1), y}; // for some reason add 1 if odd
+			*size = (t_point){x + (x & 1), y}; // for some reason add 1 if odd
 			printf("%zd: found %d * %d img %d bit \n", str->i, size->x, size->y, 8);
 			str->i += 16;
-			return 8;
+			return (8);
 		}
 	}
 	fprintf(stderr, "not found\n");
-	return 0;
+	return (0);
 }
 
 static t_stream	str = {};
@@ -198,27 +214,27 @@ static t_stream	str = {};
 int	draw(t_image *img)
 {
 	if (img->n_bit == 8)
-		for (size_t i=0; i<(size_t)(img->size.y * img->size.x); i+=1)
+		for (size_t i = 0; i < (size_t)(img->size.y * img->size.x); i += 1)
 		{
 			uint8_t		b = ft_getc(&str);
 			uint8_t		g = ft_getc(&str);
 			uint8_t		r = ft_getc(&str);
 			size_t		n = img->bits_px / CHAR_BIT;
-			img->addr[i*n+0] = b;
-			img->addr[i*n+1] = g;
-			img->addr[i*n+2] = r;
+			img->addr[i * n + 0] = b;
+			img->addr[i * n + 1] = g;
+			img->addr[i * n + 2] = r;
 		}
 	else if (img->n_bit == 5)
-		for (size_t i=0; i<(size_t)(img->size.y * img->size.x); i+=1)
+		for (size_t i = 0; i < (size_t)(img->size.y * img->size.x); i += 1)
 		{
-			uint16_t	color = ft_getc(&str)<<0 | ft_getc(&str)<<8;
-			uint8_t		r = (color>>10 & 0x1f)*8;
-			uint8_t		g = (color>>5 & 0x1f)*8;
-			uint8_t		b = (color>>0 & 0x1f)*8;
+			uint16_t	color = ft_getc(&str) << 0 | ft_getc(&str) << 8;
+			uint8_t		r = (color >>10 & 0x1f) * 8;
+			uint8_t		g = (color >> 5 & 0x1f) * 8;
+			uint8_t		b = (color >> 0 & 0x1f) * 8;
 			size_t		n = img->bits_px / CHAR_BIT;
-			img->addr[i*n+0] = b;
-			img->addr[i*n+1] = g;
-			img->addr[i*n+2] = r;
+			img->addr[i * n + 0] = b;
+			img->addr[i * n + 1] = g;
+			img->addr[i * n + 2] = r;
 		}
 	else
 		return (-1);
@@ -230,31 +246,30 @@ int	main(int ac, char *arg[])
 {
 	{
 		if (ac != 2)
-			return fprintf(stderr, "usage: %s MFA_file\n", arg[0]), 0;
-		char	*ext = strrchr(arg[1], '.');
+			return (fprintf(stderr, "usage: %s MFA_file\n", arg[0]), 0);
+		const char	*ext = strrchr(arg[1], '.');
 		if (!ext || 0 != strcmp(ext, ".mfa"))
-			return fprintf(stderr, "error: %s: not a .mfa file\n", arg[1]), 1;
-		FILE *fp = or_exit(fopen(arg[1], "r"), "failed to open file");
+			return (fprintf(stderr, "error: %s: not a .mfa file\n", arg[1]), 1);
+		FILE	*fp = or_exit(fopen(arg[1], "r"), "failed to open file");
 		str.len = fread(str.ptr, 1, BUFFER_SIZE, fp);
 		fclose(fp);
 		display_header(&str);
 	}
-
 	{
-		void	*mlx = or_exit(mlx_init(), "mlx_init");
+		void *const	mlx = or_exit(mlx_init(), "mlx_init");
 		static t_image	images[MAX_IMAGE] = {};
-		for (size_t i=0; i < MAX_IMAGE; i++)
+		for (size_t i = 0; i < MAX_IMAGE; i++)
 		{
-			t_image	*img = &images[i];
+			t_image *const	img = &images[i];
 			img->mlx = mlx;
 			img->index = i;
 			img->n_bit = seek_image(&str, &img->size);
 			if (img->n_bit <= 0)
 				break;
 			char	n_str[20];
-			img->win = or_exit(mlx_new_window(img->mlx, img->size.x, img->size.y, (snprintf(n_str, sizeof(n_str),  "%zu", i), n_str)), "mlx_new_window");
+			img->win = or_exit(mlx_new_window(img->mlx, img->size.x, img->size.y, (snprintf(n_str, sizeof(n_str), "%zu", i), n_str)), "mlx_new_window");
 			img->img = or_exit(mlx_new_image(img->mlx, img->size.x, img->size.y), "mlx_new_image");
-			img->addr = (uint8_t*)mlx_get_data_addr(img->img, &img->bits_px, &img->size_line, &img->endian);
+			img->addr = (uint8_t *)mlx_get_data_addr(img->img, &img->bits_px, &img->size_line, &img->endian);
 			mlx_hook(img->win, DestroyNotify, StructureNotifyMask, &destroy_handler, img);
 			mlx_key_hook(img->win, &key_handler, img);
 			draw(img);
